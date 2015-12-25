1f64ddc43f78377a3ce52ccb29881b098a645913
Praveer Gupta <praveerg@thoughtworks.com>
2015-12-25 08:32:34 +0530

    Complete first lesson

--- app/src/main/java/experiments/android/app/MainActivityFragment.java
+++ app/src/main/java/experiments/android/app/MainActivityFragment.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
@@ -17,6 +19,24 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> weekForecast = Arrays.asList(
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        );
+
+        ArrayAdapter<String> forecastAdapter = new ArrayAdapter<>(getActivity(),
+                R.layout.list_item_forecast, R.id.forecastListItemTextView, weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView forecastListView = (ListView) rootView.findViewById(R.id.forecastListView);
+        forecastListView.setAdapter(forecastAdapter);
+
+        return rootView;
     }
 }
